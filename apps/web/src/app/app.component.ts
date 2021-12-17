eef142e2ad4a8c8c8aed3d12890b5fa41dd28db6
Sasha9a <aleksandr_dovgiyy@mail.ru>
2021-12-18 01:10:39 +0300

    Исправление ошибок

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Message } from '@crm-bookstore/api-interfaces';
 
 @Component({
   selector: 'crm-bookstore-root',
@@ -8,6 +7,6 @@ import { Message } from '@crm-bookstore/api-interfaces';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
+  hello$ = this.http.get<any>('/api/hello');
   constructor(private http: HttpClient) {}
 }
