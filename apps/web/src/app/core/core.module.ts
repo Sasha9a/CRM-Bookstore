154953f75158714cb52139dfc286eada139bb48b
Sasha9a <aleksandr_dovgiyy@mail.ru>
2022-01-14 23:52:46 +0300

    Авторизация

--- apps/web/src/app/core/core.module.ts
+++ apps/web/src/app/core/core.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from "@crm/web/core/app.component";
+import { LoginComponent } from "@crm/web/modules/user/components/login/login.component";
 import { SharedModule } from "@crm/web/shared/shared.module";
 import { ConfirmationService, MessageService } from "primeng/api";
 
+/** Массив URL маршрутов с настройками */
 const routes: Routes = [
-
+  {
+    path: 'login',
+    component: LoginComponent
+  }
 ];
 
 @NgModule({
---
+++ apps/web/src/app/modules/user/components/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+/** Компонент авторизации */
+@Component({
+  selector: 'crm-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
