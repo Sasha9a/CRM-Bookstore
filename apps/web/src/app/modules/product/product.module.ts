c682cd3350696a2c6211c7f7994d23c5d6752033
Sasha9a <aleksandr_dovgiyy@mail.ru>
2022-02-06 14:50:51 +0300

    Обновил зависимости front Товары

--- apps/web/src/app/modules/product/product.module.ts
+++ apps/web/src/app/modules/product/product.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductFormComponent } from './dumbs/product-form/product-form.component';
+import { ProductAddComponent } from './components/add/product-add.component';
+import { ProductEditComponent } from './components/edit/product-edit.component';
+import { ProductListComponent } from './components/list/product-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductFormComponent,
+    ProductAddComponent,
+    ProductEditComponent,
+    ProductListComponent
+  ],
   imports: [
     CommonModule
   ]
