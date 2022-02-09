5515bf5872be375714c3e01dee59c3a8c45dd0f2
adovgiy <adovgiy@zaoblako.ru>
2022-02-09 18:34:58 +0300

    DTO зарплат

---
+++ libs/shared/src/dtos/salary/salary.info.dto.ts
@@ -0,0 +1,44 @@
+import { UserDto } from "@crm/shared/dtos/user/user.dto";
+import { Expose } from "class-transformer";
+
+/** DTO информации о зарплате */
+@Expose()
+export class SalaryInfoDto {
+
+  /** Сотрудник */
+  @Expose()
+  public user: UserDto;
+
+  /** Отработанные дни */
+  @Expose()
+  public daysWorked: number;
+
+  /** Премия */
+  @Expose()
+  public premium: number;
+
+  /** Штраф */
+  @Expose()
+  public fine: number;
+
+  /** Дни пропущенные по болезни */
+  @Expose()
+  public sickDays: number;
+
+  /** Дни по отпуску */
+  @Expose()
+  public vacationDays: number;
+
+  /** Описание */
+  @Expose()
+  public description: string;
+
+  /** Итоговая сумма зарплаты сотруднику */
+  @Expose()
+  public sumEmployee: number;
+
+  /** Итоговая сумма зарплаты с налогами */
+  @Expose()
+  public sumTaxes: number;
+
+}
---
+++ libs/shared/src/schemas/salary.schema.ts
@@ -0,0 +1,36 @@
+import { SalaryInfoDto } from "@crm/shared/dtos/salary/salary.info.dto";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import * as mongoose from "mongoose";
+import { Document } from "mongoose";
+
+/** Схема БД зарплат */
+@Schema({ versionKey: false })
+export class Salary extends Document {
+
+  /** Дата начисления зарплаты */
+  @Prop({ required: true })
+  public date: Date;
+
+  /** Период начала работ */
+  @Prop({ required: true })
+  public dateFrom: Date;
+
+  /** Период окончания работ */
+  @Prop({ required: true })
+  public dateTo: Date;
+
+  /** Описание */
+  @Prop()
+  public description: string;
+
+  /** Информация о зарплате списка сотрудников */
+  @Prop({ type: [mongoose.Schema.Types.Mixed], required: true })
+  public info: SalaryInfoDto[];
+
+  /** Итого сумма зарплаты */
+  @Prop({ required: true })
+  public sum: number;
+
+}
+
+export const SalarySchema = SchemaFactory.createForClass(Salary);
