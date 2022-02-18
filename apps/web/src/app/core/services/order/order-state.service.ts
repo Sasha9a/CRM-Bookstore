570c97b86b503753f0cf19c96e15ed0251f0bdf5
adovgiy <adovgiy@zaoblako.ru>
2022-02-18 16:53:25 +0300

    back Заказ

--- apps/web/src/app/core/services/order/order-state.service.ts
+++ apps/web/src/app/core/services/order/order-state.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+import { OrderDto } from "@crm/shared/dtos/order/order.dto";
 import { BaseStateService } from "@crm/web/core/services/base-state.service";
 import { OrderService } from "@crm/web/core/services/order/order.service";
+import { Observable } from "rxjs";
 
 /** Сервис для запросов по заказам в API */
 @Injectable({
@@ -11,6 +13,15 @@ export class OrderStateService extends BaseStateService {
   public constructor(private readonly orderService: OrderService) {
     super();
     this.baseService = orderService;
+  }
+
+  /**
+   * Get-запрос на получение последних заказов по конкретному товару
+   * @param productId ID товара
+   * @return Возвращает массив заказов
+   */
+  public getAllByProduct(productId: string): Observable<OrderDto[]> {
+    return this.orderService.getAllByProduct(productId);
   }
 
 }
--- apps/web/src/app/core/services/order/order.service.ts
+++ apps/web/src/app/core/services/order/order.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { OrderDto } from "@crm/shared/dtos/order/order.dto";
 import { BaseService } from "@crm/web/core/services/base.service";
+import { Observable } from "rxjs";
 
 /** Сервис для запросов по заказам в API */
 @Injectable({
@@ -9,5 +11,14 @@ export class OrderService extends BaseService {
 
   /** URL заказов в API */
   protected override baseUrl = '/order';
+
+  /**
+   * Get-запрос на получение последних заказов по конкретному товару
+   * @param productId ID товара
+   * @return Возвращает массив заказов
+   */
+  public getAllByProduct(productId: string): Observable<OrderDto[]> {
+    return this.http.get<OrderDto[]>(`${this.baseUrl}/product/${productId}`);
+  }
 
 }
