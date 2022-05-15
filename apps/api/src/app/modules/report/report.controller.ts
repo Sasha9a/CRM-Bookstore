bcaddd5c29435df3bd540ac31b62280f89f06c6b
Sasha9a <aleksandr_dovgiyy@mail.ru>
2022-05-15 12:14:06 +0300

    Часовые пояса

--- apps/api/src/app/modules/report/report.controller.ts
+++ apps/api/src/app/modules/report/report.controller.ts
@@ -60,7 +60,7 @@ export class ReportController {
     let salaries: SalaryDto[];
     const filterDates = {
       date: {
-        $gte: moment(queryParams.from).utcOffset('-03:00').toISOString(),
+        $gte: moment(queryParams.from).utcOffset('-03:00').toISOString(true),
         $lte: moment(queryParams.to).toISOString()
       }
     };
